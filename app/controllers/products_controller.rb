21ad0d3b1b09b8eef2badb9c23134f717f04b1a7
Lianna Newman <liannanewman@gmail.com>
2016-07-21 20:28:12 -0400

    Thursday 7/21 updates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,25 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
     render 'index.html.erb'
+  end
+
+  def show
+    @products = Product.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+
+  def create
+    Product.create(
+    name: params[:name],
+    price: params[:price],
+    image: params[:image],
+    description: params[:description]
+    )
+    render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  gets '/product' => 'products#index'
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
+  get '/products/:id' => 'products#show'
 end
